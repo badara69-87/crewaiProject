9914ea5656698f35f690b8018c51259cc79c5201
badara69-87 <badara6987@gmail.com>
2026-02-02 15:12:55 +0000

    Assignation des roles aux agents

--- src/agents.py
+++ src/agents.py
@@ -1,12 +1,14 @@
 from textwrap import dedent
-from crewai import Agents
+from crewai import Agent
+
+from tools import ExaSearchToolset
 
 class MeetingPrepAgents():
     def research_agent (self):
         return Agent(
             role="Research Specialist",
             goal='Conduct thorough research on people and companies involved in the meeting', 
-            tools=[],
+            tools=ExaSearchToolset.tools(),
             backstory=dedent(f"""\
             As a researcher Specialist, your mission is to uncover detailed information about the individuals and entities in the meeting.
             Your insights will lay the groundwork for strategic meeting preparation.
@@ -18,7 +20,7 @@ class MeetingPrepAgents():
         return Agent(
             role="Industry Analyst",
             goal='Analyse the current trends, challenges, and opportinuties', 
-            tools=[],
+            tools=ExaSearchToolset.tools(),
             backstory=dedent(f"""\
             As a industry analyst, your analysis will identify key trends, challenges facing the industry, and potential opportinuties
             that could be leverage during the meeting for strategic advantage
@@ -29,8 +31,7 @@ class MeetingPrepAgents():
     def meeting_strategy_agent (self):
         return Agent(
             role="Meeting Strategy Advisor",
-            goal='Develop talking points, questions, and strategic angles for the meeting', 
-            #tools=[],
+            goal='Develop talking points, questions, and strategic angles for the meeting',
             backstory=dedent(f"""\
             As strategy Advisor, your expertise will guide the development of talking points, insightful questions, and strategic angles 
             to ensure the meeting's objectives are achieved
@@ -42,7 +43,6 @@ class MeetingPrepAgents():
         return Agent(
             role="Briefing Coordinator",
             goal='Compile all gathered information into a concise, informative briefing document', 
-            tools=[],
             backstory=dedent(f"""\
             As a Briefing coordonator, your role is to consolidate the research, analysis, and strategic insights
             """),
